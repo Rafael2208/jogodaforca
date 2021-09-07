611262c7ee3483d646c3e934be1a834ea08719fd
Rafael <rafaelviana001@yahoo.com>
2021-09-06 21:32:22 -0300

    alterações iniciais

--- jogodaforca.cpp
+++ jogodaforca.cpp
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+int main () {
+ int opcao = 0
+
+ cout << "BEM VINDO AO JOGO";
+ cout << "\n1 - JOGAR";
+ count << "\n2 - SOBRE";
+ count << "\n3 - SOBRE";
+ count << "\nESCOLHA UMA OPCAO E TECLE - ENTER -";
+
+ cin >> opcao;
+
+ switch(opcao){
+ case 1 :
+     break;
+ case 2 :
+     break;
+ case 3 :
+     break;
+ }
+
+ return 0;
+
+}
